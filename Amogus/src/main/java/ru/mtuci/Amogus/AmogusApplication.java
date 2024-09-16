b381ffce56eaa61b3f01e50231677bbfa9fb91ed
Imposter <platon.msu@gmail.com>
2024-09-16 18:04:48 +0300

    init

---
+++ Amogus/src/main/java/ru/mtuci/Amogus/AmogusApplication.java
@@ -0,0 +1,13 @@
+package ru.mtuci.Amogus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmogusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmogusApplication.class, args);
+	}
+
+}
---
+++ Amogus/src/test/java/ru/mtuci/Amogus/AmogusApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.Amogus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmogusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
